80633d5e5427ef33d39c40341b253f48449bc836
edwak97 <ekilba@yahoo.com>
2025-12-08 17:51:20 +0300

    Day 8, Part 1

---
+++ day8/sol_part1.py
@@ -0,0 +1,28 @@
+def updateJunction(box_to_junction, new_junction, old_junction):
+    for i, junction in enumerate(box_to_junction):
+        if junction == old_junction:
+            box_to_junction[i] = new_junction
+if __name__ == '__main__':
+    lines = []
+    with open("test_file2") as file:
+        lines = [tuple(map(int, line.strip().split(','))) for line in file.readlines()]
+    distances = []
+    box_to_junction = [i for i in range(len(lines))]
+    for i in range(len(lines)):
+        for k in range(i+1, len(lines)):
+            dist = (lines[i][0] - lines[k][0])**2 + (lines[i][1] - lines[k][1])**2 + (lines[i][2] - lines[k][2])**2
+            distances.append((dist, i, k))
+    distances.sort(key = lambda x:x[0])
+    i = 0
+    for _, box0, box1 in distances:
+        if i == 1000:
+            break
+        updateJunction(box_to_junction, box_to_junction[box0], box_to_junction[box1])
+        i += 1
+    junction_size = [0] * len(lines)
+    for j_id in box_to_junction:
+        junction_size[j_id] += 1
+    junction_size.sort()
+    #part1
+    print(junction_size[-1] * junction_size[-2] * junction_size[-3])
+
